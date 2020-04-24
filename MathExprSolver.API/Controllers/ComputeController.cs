315f48d48e8c753b597c2c54c7470d015c1821da
Aleš Kopecký <kopeckyales@outlook.com>
2020-04-24 18:04:59 +0200

    add solution and projects

---
+++ MathExprSolver.API/Controllers/ComputeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MathExprSolver.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ComputeController : ControllerBase
+    {
+        private readonly ILogger<ComputeController> _logger;
+
+        public ComputeController(ILogger<ComputeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public float Get(string expr)
+        {
+            return 0f;
+        }
+    }
+}
